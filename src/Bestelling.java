b498da0334aa16cb6ee18d72a84a1534e61cd62e
Vocalistina <nadine.vaesen@gmail.com>
2020-01-29 20:06:30 +0100

    changed some things in bestelling

--- src/Bestelling.java
+++ src/Bestelling.java
@@ -2,7 +2,7 @@ import java.sql.Array;
 import java.util.Scanner;
 
 public class Bestelling {
-    Array[] bestelling = new Array[100];
+    Product[] bestelling = new Product[100];
     int productNummer = 1000;
 
     //CONSTRUCTORS
@@ -12,45 +12,10 @@ public class Bestelling {
     }
 
     //METHODES
-    public Product voegProductToe(Product nieuwProduct) {
-        Product toevoegingLijst = null;
-        Scanner toevoegingProduct = new Scanner(System.in);
-        System.out.println("NIEUW PRODUCT TOEVOEGEN" );
-        System.out.println("Merk: ");
-        String merk = toevoegingProduct.nextLine();
-        toevoegingProduct.nextLine();
-        productNummer = productNummer +1;
-        System.out.println("Naam: ");
-        String naam = toevoegingProduct.nextLine();
-        toevoegingProduct.nextLine();
-        System.out.println("Volume: ");
-        int volume = toevoegingProduct.nextInt();
-        System.out.println("Prijs iBTW: ");
-        double prijs = toevoegingProduct.nextDouble();
-        System.out.println("Welk soort product wil je toevoegen? 1= Aftershave, 2= Deodorant, 3=Parfum");
-        String gekozenProduct = toevoegingProduct.nextLine();
-        toevoegingProduct.nextLine();
-        if (gekozenProduct.equals("1")) {
-            System.out.println("Welke soort Deotype is dit, VAPO of STICK? ");
-            DeoType soort = DeoType.valueOf(toevoegingProduct.nextLine());
-            toevoegingProduct.nextLine();
-            toevoegingLijst = new AfterShave(productNummer,merk,naam,volume,prijs,soort);
-        } else if (gekozenProduct.equals("2")){
-            System.out.println("Welke soort Deotype is dit, VAPO of STICK? ");
-            DeoType soort = DeoType.valueOf(toevoegingProduct.nextLine());
-            toevoegingProduct.nextLine();
-            toevoegingLijst = new Deodorant(productNummer,merk,naam,volume,prijs,soort);
-        } else if (gekozenProduct.equals("3")) {
-            toevoegingLijst = new Parfum(productNummer,merk,naam,volume,prijs);
-        } else {
-            System.out.println("Dit product bestaat niet.");
-        }
-        if (equals(toevoegingLijst)) {
-            System.out.println("Het product is reeds toegevoegd.");
-        }
-        return  toevoegingLijst;
+    public void voegProductToe(Product nieuwProduct) {
 
-    }/*
+    }
+    /*
     public void verwijderProduct(int productNummer) {
         Scanner productverwijderen = new Scanner(System.in);
         System.out.println("PRODUCT VERWIJDEREN\n" +
@@ -58,27 +23,26 @@ public class Bestelling {
         int teVerwijderenProductnummer = productverwijderen.nextInt();
         for (int nummer :
              ) {
-            
+
         }
       */
     public void toonLijst() {
-        for (int i = 0; i < bestelling.length ; i++) {
-            super.toString();
+        for (Product i : bestelling) {
+            System.out.println(i.toString());
         }
     }
     public void toonPerMerk(){
         super.toString();
     }
 
-    /*
     public void toonParfums(){
-        for (int i = 0; i < bestelling.length ; i++) {
-            while (bestelling.getClass() == Parfum) {
-
+        for (Product parfum : bestelling) {
+            if (parfum instanceof Parfum) {
+                System.out.println(parfum.toString());
             }
+        }
         }
 
 
-        }*/
     }
 
--- src/WinkelApp.java
+++ src/WinkelApp.java
@@ -8,15 +8,16 @@ public class WinkelApp {
 		for (Product artikel : lijst) {
 			bestelling.voegProductToe(artikel);
 		}
+
 		bestelling.toonLijst();
 
-		System.out.println("\nVan het merk Georgio Armani:");
-		bestelling.toonPerMerk("Georgio Armani");
+		/*System.out.println("\nVan het merk Georgio Armani:");
+		bestelling.toonPerMerk("Georgio Armani");*/
 
 		System.out.println("\nAlle Parfums:");
 		bestelling.toonParfums();
-
-		System.out.printf("\nTotale prijs: �%.2f", bestelling.totalePrijs());
+/*
+		System.out.printf("\nTotale prijs: �%.2f", bestelling.totalePrijs());*/
 
 	}
 }
